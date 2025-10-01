f66d0e621c5502db31c5f22d119972539eafd746
lowercase-tousif <toisiftasrik@gmail.com>
2025-10-01 23:04:28 +0600

    Project Done

---
+++ Projects/todo-with-zod/middlewares/schema.validation.middleware.js
@@ -0,0 +1,44 @@
+const { todoSchema, updateTodoSchema } = require("../schema/zod.schema");
+
+// Custom middlewares
+function todoValidation(req, res, next) {
+  const checkerData = todoSchema.safeParse(req.body);
+
+  if (!checkerData.success) {
+    const errors = checkerData.error.issues.map((err) => ({
+      field: err.path.join("."),
+      message: err.message,
+    }));
+
+    return res.status(422).json({
+      message: "Registration Failed",
+      errors,
+    });
+  }
+
+  req.validatedTodo = checkerData.data;
+  next();
+}
+
+// Custom updating middlewares
+function validateUpdateData(req, res, next) {
+  const data = req.body;
+  const checkerData = updateTodoSchema.safeParse(data);
+
+  if (!checkerData.success) {
+    const errors = checkerData.error.issues.map((err) => ({
+      field: err.path.join("."),
+      message: err.message,
+    }));
+
+    return res.status(422).json({
+      message: "Updating failed due to validation errors",
+      errors,
+    });
+  }
+
+  req.validatedData = checkerData.data;
+  next();
+}
+
+module.exports = { todoValidation, validateUpdateData };
