fb91b65feeeb2734368c1a518d9536c98e03be6c
lowercase-tousif <toisiftasrik@gmail.com>
2025-10-03 17:29:55 +0600

    feat: Middleware separate folder created

---
+++ Projects/blog-with-zod/middleware/post.validation.middleware.js
@@ -0,0 +1,44 @@
+const {
+  BLOGSCHEMA,
+  updateBlogPostSchema,
+} = require("../schema/blog.validation.schema");
+
+
+// Custom middleware made by me
+function validatePost(req, res, next) {
+  const resultData = BLOGSCHEMA.safeParse(req.body);
+
+  if (!resultData.success) {
+    const errors = resultData.error.issues.map((err) => ({
+      field: err.path.join("."),
+      message: err.message,
+    }));
+
+    return res.status(422).json({
+      message: "Post add failed",
+      errors,
+    });
+  }
+
+  req.validatePostData = resultData.data;
+  next();
+}
+
+function validateUpdatePostData(req, res, next) {
+  const resultData = updateBlogPostSchema.safeParse(req.body);
+
+  if (!resultData.success) {
+    const errors = resultData.error.issues.map((err) => ({
+      field: err.path.join("."),
+      message: err.message,
+    }));
+
+    return res.status(422).json({
+      message: "Updating failed due to validation errors",
+    });
+  }
+  req.validatePostData = resultData.data;
+  next();
+}
+
+module.exports = { validatePost, validateUpdatePostData };
