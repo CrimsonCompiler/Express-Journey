32275d81f7d3882e81311201820c9ff0fc35763b
lowercase-tousif <toisiftasrik@gmail.com>
2025-10-03 17:30:28 +0600

    refactor: removed the custom middleware codes

--- Projects/blog-with-zod/index.js
+++ Projects/blog-with-zod/index.js
@@ -6,43 +6,6 @@ const app = express();
 app.use(express.json());
 
 
-// Custom middleware made by me
-function validatePost(req, res, next) {
-  const resultData = BLOGSCHEMA.safeParse(req.body);
-
-  if (!resultData.success) {
-    const errors = resultData.error.issues.map((err) => ({
-      field: err.path.join("."),
-      message: err.message,
-    }));
-
-    return res.status(422).json({
-      message: "Post add failed",
-      errors,
-    });
-  }
-
-  req.validatePostData = resultData.data;
-  next();
-}
-
-function validateUpdatePostData(req, res, next) {
-  const resultData = updateBlogPostSchema.safeParse(req.body);
-
-  if (!resultData.success) {
-    const errors = resultData.error.issues.map((err) => ({
-      field: err.path.join("."),
-      message: err.message,
-    }));
-
-    return res.status(422).json({
-      message: "Updating failed due to validation errors",
-    });
-  }
-  req.validatePostData = resultData.data;
-  next();
-}
-
 // BLOGS LIST []
 let blogsList = [];
 
