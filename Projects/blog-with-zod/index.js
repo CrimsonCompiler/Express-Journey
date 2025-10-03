6150808b6621f7ecfb6a1c1fd7d73b71de815eb3
lowercase-tousif <toisiftasrik@gmail.com>
2025-10-03 17:45:50 +0600

    refactor: removed the controller code and removed blogsList array imported all the middlewares and controller | Now working fine

--- Projects/blog-with-zod/index.js
+++ Projects/blog-with-zod/index.js
@@ -1,94 +1,28 @@
 const express = require("express");
-const uuid = require("uuid");
+const {
+  fetchAllBlogPost,
+  fetchBlogPostById,
+  postBlogPost,
+  updateBlogPost,
+  deleteBlogPost,
+} = require("./controller/post.controller");
 
+// required middlewares
+const {
+  validatePost,
+  validateUpdatePostData,
+} = require("./middleware/post.validation.middleware");
 const app = express();
-
 app.use(express.json());
 
+app.get("/posts", fetchAllBlogPost);
 
-// BLOGS LIST []
-let blogsList = [];
+app.get("/posts/:postId", fetchBlogPostById);
 
-app.get("/posts", (req, res) => {
-  if (blogsList.length === 0) {
-    return res.status(204).send("No blogs post found");
-  }
+app.post("/posts", validatePost, postBlogPost);
 
-  return res.status(200).json(blogsList);
-});
+app.put("/posts/:postId", validateUpdatePostData, updateBlogPost);
 
-app.get("/posts/:postId", (req, res) => {
-  const postId = String(req.params.postId);
-
-  const resultData = blogsList.find((post) => post.postId === postId);
-
-  if (!resultData) {
-    return res.status(404).json({
-      message: "Post not found",
-    });
-  }
-
-  return res.status(200).json({
-    post: resultData,
-  });
-});
-
-app.post("/posts", validatePost, (req, res) => {
-  const validatedPostData = req.validatePostData;
-
-  // generating a new id for a specific post
-  const uniqueId = uuid.v4();
-
-  // creating a newPost with the uniqueId
-  const newPost = {
-    postId: uniqueId,
-    ...validatedPostData,
-  };
-
-  blogsList.push(newPost);
-
-  res.status(200).json({
-    message: "Post add successfully",
-    post: newPost,
-  });
-});
-
-app.put("/posts/:postId", validateUpdatePostData, (req, res) => {
-  const postId = req.params.postId;
-
-  const validatedPostData = req.validatePostData;
-  const postFoundIndex = blogsList.findIndex((post) => post.postId === postId);
-
-  if (postFoundIndex === -1) {
-    return res.status(404).json({
-      message: "Post not found",
-    });
-  }
-
-  const updatePost = { ...blogsList[postFoundIndex], ...validatedPostData };
-  blogsList[postFoundIndex] = updatePost;
-
-  return res.status(200).json({
-    message: "Post updated successfully",
-    post: updatePost,
-  });
-});
-
-app.delete("/posts/:postId", (req, res) => {
-  const postId = req.params.postId;
-
-  const resultData = blogsList.some((post) => post.postId === postId);
-
-  if (!resultData) {
-    return res.status(404).json({
-      message: "Post not found",
-    });
-  }
-
-  blogsList = blogsList.filter((post) => post.postId !== postId);
-  return res.status(200).json({
-    message: "Post deleted successfully",
-  });
-});
+app.delete("/posts/:postId", deleteBlogPost);
 
 app.listen(3000);
