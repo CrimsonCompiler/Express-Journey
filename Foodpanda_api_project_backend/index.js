2abb72fc1cb24169d766f9855a1b5956ad20ec17
CrimsonCompiler <toisiftasrik@gmail.com>
2025-10-14 17:03:48 +0600

    feat: Fetching foods using a vendor Id

--- Foodpanda_api_project_backend/index.js
+++ Foodpanda_api_project_backend/index.js
@@ -37,4 +37,33 @@ app.post("/api/restaurants", async (req, res) => {
   }
 });
 
+// IMPORTANT HEADERS FOR GETTING DETAILS OF EACH RESTAURANT
+const headersForDetails = {
+  "User-Agent":
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+  "Accept-Language": "en",
+  "Content-Type": "application/json",
+  "X-FP-API-KEY": "android",
+  "X-Perseus-Region": "BD",
+};
+
+// Getting restaurants details i guess
+app.get("/api/restaurant-details/:vendorId", async (req, res) => {
+  const vendorId = req.params.vendorId;
+
+  const url = `https://bd.fd-api.com/api/v5/vendors/${vendorId}?include=menus,bundles,multiple_discounts&language_id=1&opening_type=delivery&basket_currency=BDT`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: headersForDetails,
+    });
+
+    const data = await response.json();
+    res.json({ data: data });
+  } catch (err) {
+    res.json({ error: err });
+  }
+});
+
 app.listen(4000);
